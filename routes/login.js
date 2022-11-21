0be52538455c6e6d4b3bda695c227e8acc72acfe
Valeria Proshachenko <vproshachenko@gmail.com>
2022-11-21 19:12:11 +0200

    [feat]: hash password

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const router = Router();
 
 router.get('/login', async (req, res) => {
@@ -20,7 +21,7 @@ router.post('/login', async (req, res) => {
 		const {email, password} = req.body;
 		const candidate = await User.findOne({email});
 		if (candidate) {
-			const areSame = password === candidate.password;
+			const areSame = await bcrypt.compare(password, candidate.password);
 			if (areSame) {
 				req.session.user = candidate;
 				req.session.isAuth = true;
@@ -49,8 +50,9 @@ router.post('/register', async (req, res) => {
 		if (candidate) {
 			res.redirect('/auth/login#login');
 		} else {
+			const hashpass = await bcrypt.hash(password, 10);
 			const user = new User({
-				email, name, password, cart: {items: []},
+				email, name, password: hashpass, cart: {items: []},
 			});
 
 			await user.save();
